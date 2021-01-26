b5d8ac6d757d8e4411d14e82222ddaac09def1db
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-01-26 14:25:37 +0100

    functionality added

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const upperButton = document.querySelector('.upper');
 const burger = document.querySelector(".burger");
-const nav = document.querySelector('nav')
+const nav = document.querySelector('nav');
+const span = document.querySelectorAll('span')
+const navLinks = nav.querySelectorAll('a')
+
+
 
 burger.addEventListener('click', function(){
     nav.classList.toggle('active');
+    burger.classList.toggle('toggle');
+
+})
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.classList.toggle('active');
+        burger.classList.toggle('toggle');
+    })
 })
 
