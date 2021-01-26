06ce30966811ed3f88bc622b5631ad10522e65db
juniorafter30 <75785576+juniorafter30@users.noreply.github.com>
2021-01-26 10:58:21 +0100

    Moving forward

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 const upperButton = document.querySelector('.upper');
+const burger = document.querySelector(".burger");
+const nav = document.querySelector('nav')
+
+burger.addEventListener('click', function(){
+    nav.classList.toggle('active');
+})
+
